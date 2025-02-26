0aee501bbffe092f6e24d8bb6b8e95e92875a989
Ausha Homer <Aushahomer@weber.mail.edu>
2025-02-25 19:20:38 -0700

    setup

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410
+# Build directory: /Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(main_testAreaOfCircle "main" "1")
+set_tests_properties(main_testAreaOfCircle PROPERTIES  _BACKTRACE_TRIPLES "/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;19;add_test;/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;0;")
+add_test(main_testAddFractions "main" "2")
+set_tests_properties(main_testAddFractions PROPERTIES  _BACKTRACE_TRIPLES "/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;20;add_test;/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;0;")
+add_test(main_testQuadraticFormula "main" "3")
+set_tests_properties(main_testQuadraticFormula PROPERTIES  _BACKTRACE_TRIPLES "/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;21;add_test;/Users/aushahomer/Desktop/School/SPRING25/CS1410_C++/MidtermCS1410/CMakeLists.txt;0;")
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include "MidtermH.h"
 
 int main(){
     cout << "hello world" << endl;
