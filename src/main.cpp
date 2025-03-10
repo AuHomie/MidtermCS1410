1230f82b1ee24139086fe0ebd2c458ac57889d8b
Ausha Homer <Aushahomer@weber.mail.edu>
2025-03-10 17:35:27 -0600

    fixing more rolls of death.

--- src/main.cpp
+++ src/main.cpp
@@ -13,54 +13,54 @@ int main() {
 
     // Main menu loop
     while (true) {
-        cout << endl << "Closet Checkout System"<< endl;
+        cout << endl << "Closet Checkout System";
       
-        cout << "1. Check Out Item"<< endl;
-        cout << "2. Return Item"<< endl;
-        cout << "3. Add New Clothing"<< endl;
-        cout << "4. Exit"<< endl;
+        cout << "1. Check Out Item'\n'";
+        cout << "2. Return Item'\n'";
+        cout << "3. Add New Clothing'\n'";
+        cout << "4. Exit'\n'";
         cout << "Choose an option: '\n'";
         cin >> choice;
 
         if (choice == 1) {
             int id;
             string person;
-            cout << "Enter Item ID you wish to check out: "<< endl;
+            cout << "Enter Item ID you wish to check out: '\n'";
             cin >> id;
-            cout << "What is your name: "<< endl;
+            cout << "What is your name:  '\n'";
             cin.ignore();
             getline(cin, person);
             myCloset.checkOutItem(id, person);
         } 
         else if (choice == 2) {
             int id;
-            cout << "Enter Item ID to return: "<< endl;
+            cout << "Enter Item ID to return: '\n'";
             cin >> id;
             myCloset.returnItem(id);
         } 
         else if (choice == 3) {
             string name;
             int type, size;
-            cout << "Enter clothing brand / nick name: "<< endl;
+            cout << "Enter clothing brand / nick name: '\n'";
             cin.ignore();
             getline(cin, name);
-            cout << "What type (0 = Top, 1 = Bottom, 2 = Jacket): "<< endl;
+            cout << "What type (0 = Top, 1 = Bottom, 2 = Jacket): '\n'";
             cin >> type;
-            cout << "What size (0 = XSM, 1 = SM, 2 = MED, 3 = LRG, 4 = XL, 5 = XXL): "<< endl;
+            cout << "What size (0 = XSM, 1 = SM, 2 = MED, 3 = LRG, 4 = XL, 5 = XXL): '\n'";
             cin >> size;
              // Input validation
              if (type >= 0 && type <= 2 && size >= 0 && size <= 5) {
                 myCloset.addClothing(name, static_cast<ClothingType>(type), static_cast<ClothingSize>(size));
             } else {
-                cout << "Invalid type or size."<< endl;
+                cout << "Invalid type or size.'\n'";
             }
         } 
         else if (choice == 4) {
-            cout << "Exiting closet program. Byeeeee!"<< endl;
+            cout << "Exiting closet program. Byeeeee!'\n'";
             break;  
         } 
         else {
-            cout << "Invalid choice. Umm.. Somethings not right. Circle the block babe! (Try again.)" << endl;
+            cout << "Invalid choice. Umm.. Somethings not right. Circle the block babe! (Try again.)'\n'" ;
         }
     }
 
