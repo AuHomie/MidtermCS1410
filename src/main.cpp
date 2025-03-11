10044bfadf3ee10d7788ce799a6cb4e7bc28de89
Ausha Homer <Aushahomer@weber.mail.edu>
2025-03-11 17:42:44 -0600

    app keeps crashing. exrta commits coming in.

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@ int main() {
 
     // Main menu loop
     while (true) {
-        cout << "'\n'Closet Checkout System\n";
+        cout << "\nCloset Checkout System\n";
         cout << "1. Check Out Item.\n";
-        cout << "2. Return Item \n";
-        cout << "3. Add New Clothing \n";
-        cout << "4. Exit'\n'";
-        cout << "Choose an option: \n";
+        cout << "2. Return Item\n";
+        cout << "3. Add New Clothing\n";
+        cout << "4. Exit\n";
+        cout << "Choose an option: ";
         cin >> choice;
 
         if (choice == 1) {
@@ -49,10 +49,10 @@ int main() {
             cin >> size;
             cout << "What color family (0 = Black, 1 = White, 2 = Red, 3 = Orange, 4 = Yellow, 5 = Green, 6 = Blue, 7 = Violet, 8 = Multi) \n";
             cin >> color;
-            cout << "Dose this item have any details? 0 = None, 1 =Dots, 2 = Stripes, 3 = Pattern, 4 = Sheer, 5 = Sequins,6 =  Leather, 7 = Latex, 8 = Lace,9 =Beaded, 10 = Embroidery, 11 = Studs\n";
+            cout << "Does this item have any details? 0 = None, 1 =Dots, 2 = Stripes, 3 = Pattern, 4 = Sheer, 5 = Sequins,6 =  Leather, 7 = Latex, 8 = Lace,9 =Beaded, 10 = Embroidery, 11 = Studs\n";
             cin >> detail;
              // Input validation
-             if (type >= 0 && type <= 2 && size >= 0 && size <= 5 && color>=0 && color<=7 && detail>=0 && detail<=11) {
+             if (type >= 0 && type <= 2 && size >= 0 && size <= 5 && color>=0 && color<=8 && detail>=0 && detail<=11) {
                 myCloset.addClothing(name, static_cast<ClothingType>(type), static_cast<ClothingSize>(size), static_cast<ColorFam>(color),static_cast<ItemDetail>(detail));
             } else {
                 cout << "Invalid input.\n";
